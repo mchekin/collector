92559e1b7d6ce43658b50b64c946221ca398dcc6
Michael Chekin <mchekin@gmail.com>
2020-04-18 17:44:52 +0200

    adding Util\ErrorHandler.

---
+++ src/Collector/Util/ErrorHandler.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Collector\Util;
+
+use Collector\IO\IOInterface;
+
+class ErrorHandler
+{
+    /**
+     * @var IOInterface
+     */
+    private static $io;
+
+    /**
+     * Register error handler.
+     *
+     * @param IOInterface|null $io
+     */
+    public static function register(IOInterface $io = null)
+    {
+        set_error_handler(array(__CLASS__, 'handle'));
+
+        error_reporting(E_ALL | E_STRICT);
+
+        self::$io = $io;
+    }
+
+    /**
+     * Error handler
+     *
+     * @param int $level Level of the error raised
+     * @param string $message Error message
+     * @param string $file Filename that the error was raised in
+     * @param int $line Line number the error was raised at
+     *
+     * @static
+     * @throws \ErrorException
+     * @return bool
+     */
+    public static function handle($level, $message, $file, $line)
+    {
+        // error code is not included in error_reporting
+        if (!(error_reporting() & $level)) {
+            return true;
+        }
+
+        if (filter_var(ini_get('xdebug.scream'), FILTER_VALIDATE_BOOLEAN)) {
+            $message .= "\n\nWarning: You have xdebug.scream enabled, the warning above may be" .
+                "\na legitimately suppressed error that you were not supposed to see.";
+        }
+
+        if ($level !== E_DEPRECATED && $level !== E_USER_DEPRECATED) {
+            throw new \ErrorException($message, 0, $level, $file, $line);
+        }
+
+        if (self::$io) {
+
+            self::$io->writeError('<warning>Deprecation Notice: ' . $message . ' in ' . $file . ':' . $line . '</warning>');
+
+            if (self::$io->isVerbose()) {
+                self::$io->writeError('<warning>Stack trace:</warning>');
+                self::$io->writeError(self::filterBacktrace(debug_backtrace()));
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param array $debugBacktrace
+     *
+     * @return array
+     */
+    private static function filterBacktrace(array $debugBacktrace)
+    {
+        $entries = array_slice($debugBacktrace, 2);
+
+        return array_filter(array_map(function ($entry) {
+
+            if (isset($entry['line'], $entry['file'])) {
+                return '<warning> ' . $entry['file'] . ':' . $entry['line'] . '</warning>';
+            }
+
+            return null;
+        }, $entries));
+    }
+}
---
+++ tests/Collector/Test/Util/ErrorHandlerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of Collector.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Collector\Test\Util;
+
+use Collector\Util\ErrorHandler;
+use Collector\Test\TestCase;
+
+/**
+ * ErrorHandler test case
+ */
+class ErrorHandlerTest extends TestCase
+{
+    public function setUp()
+    {
+        ErrorHandler::register();
+    }
+
+    public function tearDown()
+    {
+        restore_error_handler();
+    }
+
+    /**
+     * Test ErrorHandler handles notices
+     */
+    public function testErrorHandlerCaptureNotice()
+    {
+        $this->setExpectedException('\ErrorException', 'Undefined index: baz');
+
+        $array = array('foo' => 'bar');
+        $array['baz'];
+    }
+
+    /**
+     * Test ErrorHandler handles warnings
+     */
+    public function testErrorHandlerCaptureWarning()
+    {
+        $this->setExpectedException('\ErrorException', 'array_merge');
+
+        array_merge(array(), 'string');
+    }
+
+    /**
+     * Test ErrorHandler handles warnings
+     * @doesNotPerformAssertions
+     */
+    public function testErrorHandlerRespectsAtOperator()
+    {
+        @trigger_error('test', E_USER_NOTICE);
+    }
+}
